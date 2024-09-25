0a88dba121f68e23de0290ad2df53efeba996682
Naif Otibi <nn.n@outlook.cl>
2024-09-25 07:04:56 +0300

    small change on dashboard router

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,44 +1,46 @@
 const jwt = require("jsonwebtoken");
 const express = require("express");
 const dashboardRouter = express.Router();
-require('dotenv').config()
+require('dotenv').config();
 
-dashboardRouter.use((req , res , next) => {
-    const authoriztion = req.headers['Authorization'];
-    if(!authoriztion){
-        res.status(401).json({
+dashboardRouter.use((req, res, next) => {
+    const authorization = req.headers['Authorization'] || req.headers['authorization']; // Check for both cases
+    if (!authorization) {
+        return res.status(401).json({
             error: {
-                status: 405,
-                message: "authorization header must be defined"
+                status: 401,
+                message: "Authorization header must be defined"
             }
-        }
-        )
+        });
     }
 
-    const token = authoriztion.split(" ")[1];
+    const token = authorization.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({
+            error: {
+                status: 401,
+                message: "Token must be provided"
+            }
+        });
+    }
 
     try {
-        const decodedToken = jwt.verify(token , process.env.SECRET_KEY);
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
         next();
     } catch (error) {
         res.status(401).json({
-            error:{
+            error: {
                 status: 401,
-                message: "invalid token"
+                message: "Invalid token"
             }
-        })
+        });
     }
+});
 
-})
-
-
-
-dashboardRouter.get("/" , (req , res) => {
+dashboardRouter.get("/", (req, res) => {
     res.json({
         fakeData: "fwklwnkvnlfjlwj"
-    })
-})
+    });
+});
 
-
-
-module.exports  = dashboardRouter;
+module.exports = dashboardRouter;
