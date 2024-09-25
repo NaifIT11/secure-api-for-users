1a66159bcc9c27abd2ec4fedbe76a4a2c32b27a9
Naif Otibi <nn.n@outlook.cl>
2024-09-25 05:06:46 +0300

    initialize first middleware at dashboard router

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,3 +3,14 @@ const express = require("express");
 const dashboardRouter = express.Router();
 
 
+dashboardRouter.use((req , res , next) => {
+    const authoriztion = req.headers['Authorization'];
+    if(!authoriztion){
+        res.status(401).json({message: "authorization header must be defined"})
+    }
+})
+
+
+
+
+module.exports  = dashboardRouter;
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const path = require("path");
 const cors = require('cors')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const dashboardRouter = require("./routes/dashboard");
 app =  express();
 
 
 
+
+
+app.use("/dashboard" , dashboardRouter);
+
+//404 route
 app.use((req , res) => {
     res.json({
         message: "get token from /login and acccess your info in /dashboard"
     })
 });
-
-
-
 
 app.listen(5000 , () => {
     console.log("server is running on port 5000")
